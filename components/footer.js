b74035a7750c4e9ec83c8098854c8c2f538fadbf
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-04-17 02:39:23 -0300

    17/04/2023
    -Se finaliza el componente footer con sus estilos

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,32 @@
+import Link from "next/link";
+import styles from "../styles/footer.module.css"
+
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <footer className={styles.footer}>
+            <div className={`contenedor ${styles.contenido}`}>
+                <nav className={styles.navegacion}>
+                    <Link href="/">
+                        Inicio
+                    </Link>
+
+                    <Link href="/nosotros">
+                        Nosotros
+                    </Link>
+
+                    <Link href="/blog">
+                        Blog
+                    </Link>
+
+                    <Link href="/tienda">
+                        Tienda
+                    </Link>
+                </nav>
+
+                <p>Todos los derechos reservados {new Date().getFullYear()} &copy;</p>
+
+            </div>
+        </footer>
      );
 }
 
