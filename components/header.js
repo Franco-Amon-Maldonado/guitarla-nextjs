c1dfca2e8898f278363df2c225c5a68dba6e0c09
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-04-17 02:03:52 -0300

    17/04/2023
    -Se hace uso del hook "useRouter"
    
    -Se finaliza el header con los estilos correspondientes

--- components/header.js
+++ components/header.js
@@ -1,20 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/header.module.css";
+import { useRouter } from "next/router";
+
 
 
 function Header() {
+    const router = useRouter()
+
     return ( 
-        <header>
-            <div className="contenedor">
+        <header className={styles.header}>
+            <div className={`contenedor ${styles.barra}`}>
+            <Link href='/'>
                 <Image src="img/logo.svg" width="300" height="300"alt="Logo"/>
-            </div>
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/tienda">Tienda</Link>
+            </Link>
+                
+
+            <nav className={styles.navegacion}>
+                <Link href="/" className={router.pathname==='/' ? styles.active : ''}>
+                    Inicio
+                </Link>
+
+                <Link href="/nosotros" className={router.pathname==='/nosotros' ? styles.active : ''}>
+                    Nosotros
+                </Link>
+
+                <Link href="/blog" className={router.pathname==='/blog' ? styles.active : ''}>
+                    Blog
+                </Link>
+
+                <Link href="/tienda" className={router.pathname==='/tienda' ? styles.active : ''}>
+                    Tienda
+                </Link>
 
             </nav>
+
+            </div>
+            
         </header>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,6 @@ export default function Home() {
       title={'Inicio'}
       description={'Tienda de guitarras, cursos, blog'}
     >
-      <Head>
-        <title>Guitar-LA Inicio</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;700;900&display=swap" rel="stylesheet"/>
-      </Head>
 
       <h1>Desde home</h1>
     </Layout>
