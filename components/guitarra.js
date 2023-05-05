84feb2f448c05adb90d490d8737ee9cfed39617f
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-05-05 13:51:56 -0300

    05/05/2023
    -Se agregan los archivos css para cada pagina

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 function Guitarra({guitarra}) {
@@ -6,6 +7,10 @@ function Guitarra({guitarra}) {
     return ( 
         <div>
            <Image src={imagen.data.attributes.formats.medium.url} width={600} height={400} alt={`Imagen de la guitarra ${nombre}`}/>
+            <h3>{nombre}</h3>
+            <p>{descripcion}</p>
+            <p>${precio}</p>
+            <Link href={`/guitarras/${url}`}>Ver producto</Link>
         </div>
      );
 }
